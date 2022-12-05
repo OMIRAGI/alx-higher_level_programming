b19d9e429ca877d619860cb712bf9f1cd2660a2b
OMIRAGI <omiragiruth@gmail.com>
2022-12-05 10:19:20 -0500

    reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
- #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    if my_list:
-        for i in reversed(my_list):
-            print("{:d}".format(i))
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return my_list
